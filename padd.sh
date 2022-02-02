04b8f2fc1601a92aeddb32c29196d36ce6661622
hollowlake <git@feartheaquapickle.com>
2022-02-01 17:59:02 -0700

    attempt to remove logo

--- padd.sh
+++ padd.sh
@@ -661,7 +661,7 @@ PrintLogo() {
     CleanEcho ""
   # For the next two, use printf to make sure spaces aren't collapsed
   elif [[ "$1" = "regular" || "$1" = "slim" ]]; then
-    CleanPrintf "${padd_logo_1}\e[0K\\n"
+    #CleanPrintf "${padd_logo_1}\e[0K\\n"
     CleanPrintf "${padd_logo_2}Pi-hole® ${core_version_heatmap}v${core_version}${reset_text}, Web ${web_version_heatmap}v${web_version}${reset_text}, FTL ${ftl_version_heatmap}v${ftl_version}${reset_text}\e[0K\\n"
     CleanPrintf "${padd_logo_3}PADD ${padd_version_heatmap}${padd_version}${reset_text}${full_status_}${reset_text}\e[0K\\n"
     CleanEcho ""
